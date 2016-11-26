78d612d5d4992964e511401cb3238cbce851972d
ion <ionix@protonmail.com>
2016-11-26 17:24:06 +0100

    fixed FindInstructionsByOperand. It returned Indexes when only 1 index was found, now it sets Index if so.

---
+++ UnpackMe2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet.Emit;
+using dnpatch;
+using dnpatch.deobfuscation;
+
+namespace UnpackMe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deobfuscation deobfuscation = new Deobfuscation("UnpackMe2.ori.exe", "UnpackMe2.deob1.exe");
+            deobfuscation.Deobfuscate();
+            deobfuscation = new Deobfuscation("UnpackMe2.deob1.exe", "UnpackMe2.deob2.exe");
+            deobfuscation.Deobfuscate();
+
+            ObfuscationPatcher obfuscationPatcher = new ObfuscationPatcher("UnpackMe2.deob2.exe", false);
+            Target[] targets = obfuscationPatcher.FindInstructionsByOperand(new[]
+            {
+                "bBbBbBbBb",
+                "AaAaA",
+                "Good"
+            });
+            foreach (var target in targets)
+            {
+                target.Instructions = new[]
+                {
+                    Instruction.Create(OpCodes.Ldstr, "ion"),
+                    Instruction.Create(OpCodes.Ldstr, "codes"),
+                    Instruction.Create(OpCodes.Ldstr, "Patched!")
+                };
+            }
+            obfuscationPatcher.Patch(targets);
+
+            targets = obfuscationPatcher.FindInstructionsByOperand(new []
+            {
+                "ameereagle"
+            });
+            foreach (var target in targets)
+            {
+                target.Instruction = Instruction.Create(OpCodes.Ldstr, "ioncodes");
+            }
+            obfuscationPatcher.Patch(targets);
+            obfuscationPatcher.Save("UnpackMe2.patched.exe");
+        }
+    }
+}
