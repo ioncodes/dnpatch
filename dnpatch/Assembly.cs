02b4d86a212b8b21f383f467a47e564dbfaa53b7
Luca Marcelli <ioncodes@users.noreply.github.com>
2017-07-14 19:49:01 +0200

    add AssemblyData for direct to access to dnlib. Implement it in Assembly.

--- dnpatch/Assembly.cs
+++ dnpatch/Assembly.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using dnlib.DotNet;
+
 namespace dnpatch
 {
     public class Assembly
     {
         public AssemblyInfo AssemblyInfo;
+        public AssemblyData AssemblyData;
 
         internal Assembly(AssemblyInfo assemblyInfo)
         {
             AssemblyInfo = assemblyInfo;
+            AssemblyData = new AssemblyData() // Load assembly data
+            {
+                Module = ModuleDefMD.Load(AssemblyInfo.Name)
+            };
         }
     }
 }
---
+++ dnpatch/AssemblyData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using dnlib.DotNet;
+
+namespace dnpatch
+{
+    public struct AssemblyData
+    {
+        public ModuleDefMD Module { get; internal set; }
+    }
+}
