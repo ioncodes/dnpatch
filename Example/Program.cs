bfb2c3cccbfe284de104ce8c7d5d725c4b584c26
Luca Marcelli <ioncodes@users.noreply.github.com>
2017-07-14 18:30:46 +0200

    refo

---
+++ Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+//using dnpatch;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
