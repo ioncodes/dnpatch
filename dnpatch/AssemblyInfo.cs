0a76506c0cbfc090d1d842ce6d2a87d1e861b8d0
Luca Marcelli <ioncodes@users.noreply.github.com>
2017-07-15 18:26:04 +0200

    add createBackup option and add Write method for Loader

--- dnpatch/AssemblyInfo.cs
+++ dnpatch/AssemblyInfo.cs
@@ -4,7 +4,9 @@ namespace dnpatch
     public struct AssemblyInfo
     {
         public bool OverwriteOriginal { get; internal set; }
+        public bool CreateBackup { get; internal set; }
         public string Name { get; internal set; }
+        public string OutputName { get; internal set; }
         public string InternalName { get; internal set; }
 
         public override string ToString()
--- dnpatch/Loader.cs
+++ dnpatch/Loader.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace dnpatch
 {
@@ -11,19 +12,41 @@ namespace dnpatch
             Assemblies = new Dictionary<string, Assembly>();    
         }
 
-        public void Initialize(string name, string fileName, bool overwriteOriginal) 
+        public void Initialize(string name, string fileName, bool overwriteOriginal, bool createBackup) 
         {
             Assemblies.Add(name, new Assembly(new AssemblyInfo()
             {
                 Name = fileName,
                 OverwriteOriginal = overwriteOriginal,
-                InternalName = name
+                InternalName = name,
+                CreateBackup = createBackup
             }));
         }
 
         public Assembly LoadAssembly(string name)
         {
             return Assemblies.TryGetValue(name, out Assembly value) ? value : null;
+        }
+
+        public void Write()
+        {
+            foreach(var assembly in Assemblies)
+            {
+                var asm = assembly.Value; // no not ASMR, ffs...
+                if(asm.AssemblyInfo.CreateBackup)
+                {
+                    File.Copy(asm.AssemblyInfo.Name, $"{asm.AssemblyInfo.Name}.bak");
+                }
+                if (asm.AssemblyInfo.OverwriteOriginal)
+                {
+                    asm.AssemblyData.Module.Write($"{asm.AssemblyInfo.OutputName}.tmp");
+                    asm.AssemblyData.Module.Dispose();
+                    File.Delete(asm.AssemblyInfo.OutputName);
+                    File.Move($"{asm.AssemblyInfo.OutputName}.tmp", asm.AssemblyInfo.OutputName);
+                    continue;
+                }
+                asm.AssemblyData.Module.Write(asm.AssemblyInfo.OutputName);
+            }
         }
     }
 }
